d6dc19fd8e2bbfbf4b9a1fae8ec32f6fd04c485d
D. Teuchert <teuchert@code-intelligence.com>
2021-10-20 16:19:16 +0200

    added docker container

--- c_cpp/API_Fuzzing/plain/fuzz_targets/API Fuzz Test.cpp
+++ c_cpp/API_Fuzzing/plain/fuzz_targets/API Fuzz Test.cpp
@@ -3,6 +3,7 @@
 
 #define HMAC_LENGTH 64
 
+//The software that we want to test is defined in this namespace
 namespace target_software {
 
   typedef struct hmac {
@@ -14,25 +15,25 @@ namespace target_software {
       error_during_hmac_calculation,
   };
 
-  return_status calculate_hmac(const uint8_t * const message, size_t len, hmac * h) {
-      if (len > 4) {
-          if (message[1] == 42) {
-              return hmac_successfully_calculated; 
-          }
-          if (message[0] == 1) {
-              if (message[1] == 2) {            
-                  if (message[2] == 3) {            
-                      if (message[3] == 4) {
-                          int x[3];
-                          int y = 3;
-                          h->hmac[0] = x[y];
-                      }   
-                  }   
-              }   
-          }
-      }
-      return error_during_hmac_calculation;
-  }
+    return_status calculate_hmac(const uint8_t * const message, size_t len, hmac * h) {
+        if (len > 4) {
+            if (message[1] == 42) {
+                return hmac_successfully_calculated; 
+            }
+            if (message[0] == 1) {
+                if (message[1] == 2) {            
+                    if (message[2] == 3) {            
+                        if (message[3] == 4) {
+                            int x[3];
+                            int y = 3;
+                            h->hmac[0] = x[y];
+                        }   
+                    }   
+                }   
+            }
+        }
+        return error_during_hmac_calculation;
+    }
 }
 
 extern "C" int FUZZ(const uint8_t *Data, size_t Size) {
